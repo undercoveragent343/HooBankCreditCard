fbb37a7d1a099dcffde3b91b8d5026b53c837565
Your Name <bosnianahmed123@gmail.com>
2021-12-05 23:53:35 -0600

    Added the rest of it

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,26 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Grow your credit <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Embarking on a journey to build your credit has never been easier. Our streamlined process empowers 
+        you to take confident strides towards a stronger credit profile through simple and effective steps.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
